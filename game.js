57cd09275648374a8df951123ccf87ee4e303c41
edengomes <edenilsonngomes21@gmail.com>
2026-05-30 12:08:48 -0300

    atualiza texto da memória para maior clareza e fluidez

--- game.js
+++ game.js
@@ -43,7 +43,7 @@ const memories = [
   {
     title: "Nossa primeira vez juntos na praia",
     date: "Quarta memória",
-    text: "Minha gatinha, nossa primeira praia juntos foi perfeita do nosso jeitinho. Sol, mar, você linda sorrindo. E eu pesando comigo mesmo: né que tá dando tudo certo KKKKKKKKKK..",
+    text: "Nossa primeira praia com o sol, mar e você linda sorrindo. E eu pesando comigo mesmo: né que tá dando tudo certo KKKKKKKKKK..",
     image: "assets/foto-4.png",
   },
   {
